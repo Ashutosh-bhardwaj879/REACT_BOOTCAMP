8f118330e705bf876d9d3851d950cabc81782a67
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2023-01-01 15:24:16 +0530

    DAY2-IGNITING OUR APP

---
+++ DAY2-IGNITING OUR APP/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1", { id: "title" }, "Heading1");
+const heading2 = React.createElement("h2", { id: "title" }, "Heading2");
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
