0479d4af6cefa5b04dc493466903c06596c241bb
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2023-01-07 15:36:10 +0530

    DAY3-HW1-Q1

---
+++ DAY3-LAYING-OUR-FOUNDATION/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// const heading1 = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h1",
+//   },
+//   "Headin One"
+// );
+
+// const heading2 = React.createElement(
+//   "h2",
+//   {
+//     id: "title",
+//     key: "h2",
+//   },
+//   "Heading two"
+// );
+
+/*• Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class "title")*/
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+    class: "title",
+    key: "container",
+  },
+  [
+    React.createElement("header", { id: "header", key: "header" }, [
+      React.createElement("h1", { id: "h1", key: "h1" }, "heading1"),
+      React.createElement("h2", { id: "h2", key: "h2" }, "heading2"),
+      React.createElement("h3", { id: "h3", key: "h3" }, "heading3"),
+    ]),
+  ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
